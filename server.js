b7ed6f62135793ce207c6dd851ae20b28b7b9da6
victorbalopes-maker <victorbalopes@gmail.com>
2026-05-01 15:04:42 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("OK");
+  res.send("OK");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando na porta", PORT);
+  console.log("Rodando na porta", PORT);
 });
